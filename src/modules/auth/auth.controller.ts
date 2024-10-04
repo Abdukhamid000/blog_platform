edaa2ee461cb7eb1bd995d6e8e211cfcb54b2f10
Abduhamid <Abdukhamid000@gmail.com>
2024-10-04 07:06:15 +0500

    feat: routes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,9 +5,49 @@ import { comparePw, hashPw } from "../../shared/utils/hashPw";
 import { requestBodyValidator } from "../../shared/validators/request-body.validator";
 import { LoginUserDTO } from "../user/dto/login-user.dto";
 import { HttpStatus } from "../../shared/enums/http-status.enum";
-import { Http } from "winston/lib/winston/transports";
 import { HttpException } from "../../shared/exceptions/http.exception";
+import jwt from "jsonwebtoken";
 
 const AuthController = Router();
+
+AuthController.post(
+  "/signup",
+  requestBodyValidator(CreateUserDTO),
+  async (req, res) => {
+    const { username, email, password } = req.body as CreateUserDTO;
+
+    const pw = hashPw(password);
+    const user = await UserService.createUser({
+      username,
+      email,
+      password: pw,
+    });
+
+    const { password: _, ...userWithoutPw } = user;
+    res.status(HttpStatus.CREATED).json(userWithoutPw);
+  }
+);
+
+AuthController.post(
+  "/login",
+  requestBodyValidator(LoginUserDTO),
+  async (req, res) => {
+    const { email, password } = req.body as LoginUserDTO;
+    const user = await UserService.getUserByEmail(email);
+    if (!user) {
+      throw new HttpException("User not found", HttpStatus.NOT_FOUND);
+    }
+
+    if (!comparePw(password, user.password)) {
+      throw new HttpException("Invalid password", HttpStatus.BAD_REQUEST);
+    }
+
+    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
+      expiresIn: "15m",
+      algorithm: "HS256",
+    });
+    res.status(HttpStatus.OK).json({ token });
+  }
+);
 
 export default AuthController;
