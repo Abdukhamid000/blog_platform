fb8b40595a081f03cc2d615e245ce45f48db8dee
Abduhamid <Abdukhamid000@gmail.com>
2024-10-04 06:57:36 +0500

    remove

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -10,38 +10,4 @@ import { HttpException } from "../../shared/exceptions/http.exception";
 
 const AuthController = Router();
 
-AuthController.post(
-  "/signup",
-  requestBodyValidator(CreateUserDTO),
-  async (req, res) => {
-    const { username, email, password } = req.body as CreateUserDTO;
-
-    const pw = hashPw(password);
-    const user = await UserService.createUser({
-      username,
-      email,
-      password: pw,
-    });
-
-    const { password: _, ...userWithoutPw } = user;
-    res.status(HttpStatus.CREATED).json(userWithoutPw);
-  }
-);
-
-AuthController.post(
-  "/login",
-  requestBodyValidator(LoginUserDTO),
-  async (req, res) => {
-    const { email, password } = req.body as LoginUserDTO;
-    const user = await UserService.getUserByEmail(email);
-    if (!user) {
-      throw new HttpException("User not found", HttpStatus.NOT_FOUND);
-    }
-
-    if (!comparePw(password, user.password)) {
-      throw new HttpException("Invalid password", HttpStatus.BAD_REQUEST);
-    }
-  }
-);
-
 export default AuthController;
